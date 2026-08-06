778bccc351d6b5b86245a3f4f62241c06ee1c5ce
zzangyoung <75447266+zzangyoung@users.noreply.github.com>
2026-08-06 15:34:02 +0900

    Create tsup.config.ts

---
+++ plugins/blank-line-breaks/tsup.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "tsup"
+
+const SINGLETON_EXTERNALS = [
+  "preact",
+  "preact/hooks",
+  "preact/jsx-runtime",
+  "preact/compat",
+  "@jackyzha0/quartz",
+  "@jackyzha0/quartz/*",
+  "vfile",
+  "vfile/*",
+  "unified",
+]
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  dts: true,
+  clean: true,
+  sourcemap: false,
+  noExternal: [/.*/],
+  external: SINGLETON_EXTERNALS,
+})
